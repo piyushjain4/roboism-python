5b52d16369c07957b8ed0bbb6c771bf923d94c61
piyushjain4 <102512290+piyushjain4@users.noreply.github.com>
2022-06-04 17:32:19 +0530

    Update robo_Q2.py

--- roboism/robo_Q2.py
+++ roboism/robo_Q2.py
@@ -2,7 +2,4 @@ def hidden_crdt_no(card_no):
     asterisk = "************"
     sliced_no = card_no[12:16]
     credit_no = asterisk +sliced_no
-    return credit_no
-
-num =hidden_crdt_no("3864778676325672")
-print(num) 
+    return credit_no 
