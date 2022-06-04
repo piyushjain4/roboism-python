58e19ad0069f970b3a99c05c719ae83d4b8ab4b6
piyushjain4 <102512290+piyushjain4@users.noreply.github.com>
2022-06-04 17:34:32 +0530

    Update robo_Q5.py

--- roboism/robo_Q5.py
+++ roboism/robo_Q5.py
@@ -3,8 +3,3 @@ def duplicate_no(array):
     for i in range(len(array) -1):
         if array[i] == array[i+1]:
             print(array[i])
-    
-
-
-arr = duplicate_no([1,5,7,3,9,4,5,2,8,6])
-print(arr)
