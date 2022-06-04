78c383e7ba86a5df2ef92f8dcbe7f319b75e01c5
piyushjain4 <102512290+piyushjain4@users.noreply.github.com>
2022-06-04 17:34:06 +0530

    Update robo_Q4.py

--- roboism/robo_Q4.py
+++ roboism/robo_Q4.py
@@ -3,12 +3,3 @@ def doubled_str(string):
     for i in range( len(string)):
         new_str += string[i]*2
     return new_str
-
-stri = doubled_str("now")
-print(stri)
-
-stri2 = doubled_str("123!")
-print(stri2)
-
-
-
